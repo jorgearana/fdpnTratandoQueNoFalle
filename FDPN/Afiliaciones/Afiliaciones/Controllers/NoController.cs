fb2e27d781b6c701125b01d7b6de1254e952a399
jorge arana <jorgearananeyra@hotmail.com>
2019-03-18 08:06:02 -0500

    Agregando un No Controller para ver donde se agregar en REPO

---
+++ FDPN/Afiliaciones/Afiliaciones/Controllers/NoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Afiliaciones.Controllers
+{
+    public class NoController : Controller
+    {
+        // GET: No
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
